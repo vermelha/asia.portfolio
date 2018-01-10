4edb06bcf064bbc8fb21a10857e2355b783f6fd8
Asia <vermelha@users.noreply.github.com>
2018-01-10 15:09:40 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,26 @@
-$(function(){
-
-
-    $("#home").on( 'click', function(){
+$(function() {
+    $("#home").on('click', function() {
         $("#container").load("pages/home.html");
-        
     });
-   
-     $("#about").on( 'click', function(){
+    $("#about").on('click', function() {
         $("#container").load("pages/about.html");
-        
     });
-
-     $("#skills").on( 'click', function(){
+    $("#skills").on('click', function() {
         $("#container").load("pages/skills.html");
-        
     });
-
-       $("#works").on( 'click', function(){
+    $("#works").on('click', function() {
         $("#container").load("pages/work.html");
-        
     });
-
-         $("#contact").on( 'click', function(){
+    $("#contact").on('click', function() {
         $("#container").load("pages/contact.html");
-        
     });
-
-        $("#contact-button").on( 'click', function(){
+    $("#contact-button").on('click', function() {
         $("#container").load("pages/contact.html");
-        
     });
-
-    
-
- //    $("#about").on( 'click', function(){
- //    $.ajax({url: "pages/about.html", dataType: 'html', success: 
- //    	function(result){
- //        $("#container").html(result);
- //    	}});
-	// });
-	
- 
+    //    $("#about").on( 'click', function(){
+    //    $.ajax({url: "pages/about.html", dataType: 'html', success: 
+    //      function(result){
+    //        $("#container").html(result);
+    //      }});
+    // });
 });
-
-
-
-
-
-

6707b7f69238a4df0fda867c2bc6c557b5b6e907
Asia <vermelha@users.noreply.github.com>
2018-01-10 15:10:11 +0200

    Update skills.js

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -2,17 +2,16 @@
 //   $('.butt').each(function(){
 //      $(this).css('transform','translate(-50%, -50%) rotateX(90deg)');
 // }, 1000);
-
-
-
 var button = $('.butt');
 var sButton = $('.sBtn');
-var i=0;
-button.on('mouseenter', function (){
-    $(this).css('transform','translate(-50%, -50%) rotateX(90deg)');
-    $(this).next().css('transform','translate(-50%, -100%) rotateX(0deg)');
-   sButton.on('mouseleave', function (){
-      $(this).css('transform','translate(-50%, -50%) rotateX(90deg)').removeAttr('style');
-      $(this).prev().css('transform','translate(-50%, -50%) rotateX(0deg)').removeAttr('style');
-  });
+var i = 0;
+button.on('mouseenter', function() {
+	$(this).css('transform', 'translate(-50%, -50%) rotateX(90deg)');
+	$(this).next().css('transform', 'translate(-50%, -100%) rotateX(0deg)');
+	sButton.on('mouseleave', function() {
+		$(this).css('transform', 'translate(-50%, -50%) rotateX(90deg)').removeAttr(
+			'style');
+		$(this).prev().css('transform', 'translate(-50%, -50%) rotateX(0deg)').removeAttr(
+			'style');
+	});
 });

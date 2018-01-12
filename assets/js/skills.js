5abbed337c6fc0b9d00630daf755fe108d767104
Asia <vermelha@users.noreply.github.com>
2018-01-12 14:00:56 +0200

    Update skills.js

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,17 +1,37 @@
-// setTimeout(function(){
-//   $('.butt').each(function(){
-//      $(this).css('transform','translate(-50%, -50%) rotateX(90deg)');
-// }, 1000);
+$(document).ready(function(){
+
 var button = $('.butt');
 var sButton = $('.sBtn');
-var i = 0;
-button.on('mouseenter', function() {
+
+
+button.mouseenter (function() {
 	$(this).css('transform', 'translate(-50%, -50%) rotateX(90deg)');
 	$(this).next().css('transform', 'translate(-50%, -100%) rotateX(0deg)');
-	sButton.on('mouseleave', function() {
-		$(this).css('transform', 'translate(-50%, -50%) rotateX(90deg)').removeAttr(
-			'style');
-		$(this).prev().css('transform', 'translate(-50%, -50%) rotateX(0deg)').removeAttr(
-			'style');
-	});
+})
+
+    window.setInterval(function() {
+       button.removeAttr('style');
+        sButton.removeAttr('style');
+    },1500);
+
+
+$('.parallelogram').each(function setAnim(){
+    $('#p1, #p8').
+            animate({left: '70%'},500).
+            animate({ left: '5%',},500,setAnim);
+    $('#p2, #p7').
+            animate({left: '60%'},500).
+            animate({ left: '5%',},500,setAnim);
+     $('#p3, #p6').
+            animate({left: '40%'},500).
+            animate({ left: '5%',},500,setAnim);
+     $('#p4').
+            animate({left: '80%'},500).
+            animate({ left: '5%',},500,setAnim);
+     $('#p5').
+            animate({left: '30%'},500).
+            animate({ left: '5%',},500,setAnim);         
+});
+
+
 });
